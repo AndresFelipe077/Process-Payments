9168712d2182515a0c2f89f212c915f3b6883799
AndresFelipe077 <pizoluligoa@gmail.com>
2025-04-28 15:52:40 -0500

    Add Currency and PaymentPlatform models with migrations for database tables

---
+++ app/Models/Currency.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Currency extends Model
+{
+
+    protected $primaryKey = 'iso';
+    protected $incrementing = false;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'iso',
+    ];
+}
