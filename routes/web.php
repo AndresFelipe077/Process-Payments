df3a7f4fa9e1ba3f96a5d9f03579f17d8764e2d0
AndresFelipe077 <pizoluligoa@gmail.com>
2025-04-28 20:25:21 -0500

    Add apis to file web

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,15 @@
 <?php
 
+use App\Http\Controllers\PaymentController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::post('payments/pay', [PaymentController::class, 'pay'])->name('pay');
+Route::post('payments/approval', [PaymentController::class, 'approval'])->name('approval');
+Route::post('payments/cancelled', [PaymentController::class, 'cancelled'])->name('cancelled');
 
 Auth::routes();
 
