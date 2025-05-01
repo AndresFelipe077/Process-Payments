5b00776ce75d175868a9c74f40bf00618baf9b9f
AndresFelipe077 <pizoluligoa@gmail.com>
2025-05-01 11:46:27 -0500

    Implement payment handling in StripeService and add approval logic

--- app/Services/StripeService.php
+++ app/Services/StripeService.php
@@ -39,11 +39,43 @@ class StripeService
         return "Bearer {$this->secret}";
     }
 
-    public function handlePayment(Request $request): Redirector | RedirectResponse {}
+    public function handlePayment(Request $request): Redirector | RedirectResponse
+    {
+        $request->validate([
+            'payment_method' => 'required',
+        ]);
 
-    public function handleApproval(): RedirectResponse {}
+        $intent = $this->createIntent($request->value, $request->currency, $request->payment_method);
 
-    public function createIntent(float|int $value, string $currency, string $paymentMethod): mixed
+        session()->put('paymentIntentId', $intent->id);
+
+        return redirect()->route('approval');
+    }
+
+    public function handleApproval(): RedirectResponse
+    {
+        if (session()->has('paymentIntentId')) {
+            $paymentIntentId = session()->get('paymentIntentId');
+
+            $confirmation = $this->confirmPayment($paymentIntentId);
+
+            if ($confirmation->status === 'succeeded') {
+                $name     = $confirmation->changes->data[0]->billing_details->name;
+                $currency = strtoupper($confirmation->currency);
+                $amount   = $confirmation->amount / $this->resolveFactor($currency);
+
+                return redirect()
+                    ->route('home')
+                    ->withSuccess(['payment' => "Thanks, {{$name}}. We received your {$amount}{$currency} payment."]);
+            }
+        }
+
+        return redirect()
+            ->route('home')
+            ->withErrors('We were unable to confirm your payment. Try again, please');
+    }
+
+    public function createIntent(float|int|string $value, string $currency, string $paymentMethod): mixed
     {
         return $this->makeRequest(
             'POST',
