4227c581d0a6b62b5b25278d6e4370a5f414a81a
AndresFelipe077 <pizoluligoa@gmail.com>
2025-05-01 10:16:21 -0500

    Add debugging output in PaymentController and enhance Stripe component styling

--- resources/views/components/stripe-collapse.blade.php
+++ resources/views/components/stripe-collapse.blade.php
@@ -1,25 +1,45 @@
 @push('style')
-    <style>
-        .StripElement {
+    <style type="text/css">
+        /**
+                 * The CSS shown here will not be introduced in the Quickstart guide, but shows
+                 * how you can use CSS to style your Element's container.
+                 */
+        .StripeElement {
             box-sizing: border-box;
+
             height: 40px;
+
             padding: 10px 12px;
+
             border: 1px solid transparent;
             border-radius: 4px;
             background-color: white;
+
             box-shadow: 0 1px 3px 0 #e6ebf1;
+            -webkit-transition: box-shadow 150ms ease;
+            transition: box-shadow 150ms ease;
+        }
+
+        .StripeElement--focus {
+            box-shadow: 0 1px 3px 0 #cfd7df;
+        }
+
+        .StripeElement--invalid {
+            border-color: #fa755a;
+        }
+
+        .StripeElement--webkit-autofill {
+            background-color: #fefde5 !important;
         }
     </style>
 @endpush
 
-<div class="container mt-5">
-    <label class="mt-3">Card details:</label>
-    <div id="cardElement" class="StripElement"></div>
-    <small class="form-text text-muted" id="cardErrors" role="alert"></small>
-</div>
+<label class="mt-3">Card details:</label>
+<div id="cardElement"></div>
+<small class="form-text text-muted" id="cardErrors" role="alert"></small>
+<input type="hidden" name="payment_method" id="paymentMethod">
 
 @push('scripts')
-
     <script src="https://js.stripe.com/v3/"></script>
     <script>
         const stripe = Stripe("{{ config('services.stripe.key') }}");
@@ -27,13 +47,48 @@
             locale: 'en'
         });
 
-        try {
-            const cardElement = elements.create('card');
-            cardElement.mount('#cardElement');
-            console.log('Card element montado');
-        } catch (e) {
-            console.error('Error montando el elemento de tarjeta:', e);
-        }
+        // try {
+        const cardElement = elements.create('card');
+        cardElement.mount('#cardElement');
+        //     console.log('Card element montado');
+        // } catch (e) {
+        //     console.error('Error montando el elemento de tarjeta:', e);
+        // }
+    </script>
 
+    <script>
+        const form = document.getElementById('paymentForm');
+        const payButton = document.getElementById('payButton');
+
+        payButton.addEventListener('click', async (e) => {
+
+            if (form.elements.payment_platform.value === "{{ $paymentPlatform->id }}") {
+
+                e.preventDefault();
+
+                const {
+                    paymentMethod,
+                    error
+                } = await stripe.createPaymentMethod(
+                    'card', cardElement, {
+                        billing_details: {
+                            "name": "{{ auth()->user()->name }}",
+                            "email": "{{ auth()->user()->email }}",
+                        }
+                    }
+                );
+
+                if (error) {
+                    const displayError = document.getElementById('cardErrors');
+                    displayError.textContent = error.message;
+                } else {
+                    const tokenInput = document.getElementById('paymentMethod');
+                    tokenInput.value = paymentMethod.id;
+                    form.submit();
+                }
+            }
+
+
+        });
     </script>
 @endpush
